09d9e296cd4fcdf96ee1810336d82244511defb5
cham-s <cc.attou@gmail.com>
2019-06-04 17:51:57 +0200

    done with map view.

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,32 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by chams on 04/06/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif

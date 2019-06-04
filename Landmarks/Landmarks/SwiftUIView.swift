19faed4e3fc6664ef6a241d611b1fec81550d5ca
cham-s <cc.attou@gmail.com>
2019-06-04 17:39:55 +0200

    image view done.

---
+++ Landmarks/Landmarks/SwiftUIView.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by chams on 04/06/2019.
+//  Copyright © 2019 Chams. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView : View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct SwiftUIView_Previews : PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
+#endif

47ddc0987c0e89c01732977656e54b57d674ef35
cham-s <cc.attou@gmail.com>
2019-06-04 19:31:42 +0200

    landmark row done.

---
+++ ListsAndNavigation/Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by chams on 04/06/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow : View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image(forSize: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkRow_Previews : PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
+#endif

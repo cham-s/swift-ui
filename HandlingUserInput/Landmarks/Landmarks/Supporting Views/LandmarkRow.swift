659c0f50633739b607dbc30b0d1832c7307f4775
cham-s <cc.attou@gmail.com>
2019-06-04 20:55:45 +0200

    created model object.

--- HandlingUserInput/Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
+++ HandlingUserInput/Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
@@ -8,12 +8,21 @@ A single row to be displayed in a list of landmarks.
 import SwiftUI
 
 struct LandmarkRow: View {
+    
     var landmark: Landmark
 
     var body: some View {
         HStack {
             landmark.image(forSize: 50)
             Text(landmark.name)
+            
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                .imageScale(.medium)
+                .foregroundColor(.yellow)
+            }
         }
     }
 }
---
+++ HandlingUserInput/Landmarks/Landmarks/UserData.swift
@@ -0,0 +1,26 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by chams on 04/06/2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    let didChange = PassthroughSubject<UserData, Never>()
+    
+    var showFavoritesOnly = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    var landmarks = landmarkData {
+        didSet {
+            didChange.send(self)
+        }
+    }
+}
